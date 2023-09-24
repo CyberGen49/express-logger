13665bf9352a413d50966eaa2409b72b7f529e5a
Cyber <cybergen49@gmail.com>
2023-09-23 22:36:04 -0500

    Listen for close event instead of finish

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const loggerOptions = {
 const logger = (opts = loggerOptions) => {
     /** @type {express.RequestHandler} */
     const handler = (req, res, next) => {
-        res.on('finish', () => {
+        const log = () => {
             const ip = opts.getIP(req)
                 || req.headers['cf-connecting-ip']
                 || req.headers['x-forwarded-for']
@@ -36,7 +36,8 @@ const logger = (opts = loggerOptions) => {
                 case '5': codeC = 'red'; break;
             }
             console.log(clc.white(dayjs().format(`YYYY-MM-DD HH:mm:ss`)), clc.cyanBright(ip), clc.yellowBright(req.method), clc[codeC](res.statusCode), clc.greenBright(req.url));
-        });
+        };
+        res.on('close', log);
         return next();
     };
     return handler;
